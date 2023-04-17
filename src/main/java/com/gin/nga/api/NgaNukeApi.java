0aebfe879a7e5d049f47a7cd0d241343b2d1c413
ginstone <hjg719@139.com>
2023-04-17 10:53:46 +0800

    nuke.php - 基础框架

---
+++ src/main/java/com/gin/nga/api/NgaNukeApi.java
@@ -0,0 +1,30 @@
+package com.gin.nga.api;
+
+import com.gin.nga.call.NgaJsonCall;
+import com.gin.nga.enums.NgaPhpApi;
+import com.gin.nga.params.nuke.NukeBaseParam;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 综合操作相关API
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : 2023/4/11 16:21
+ */
+@RequiredArgsConstructor
+public class NgaNukeApi {
+    private final NgaClient client;
+
+    //todo
+
+
+
+    public NgaJsonCall<?> getUserInfo(Long uid,String username){
+        return null;
+    }
+
+
+    public <T> NgaJsonCall<T> nuke(NukeBaseParam param,Class<T> responseClass){
+        return client.callJson(NgaPhpApi.nuke,param,null,responseClass);
+    }
+}   
---
+++ src/main/java/com/gin/nga/params/nuke/NukeBaseParam.java
@@ -0,0 +1,26 @@
+package com.gin.nga.params.nuke;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * nuke.php 基础参数
+ * @author : ginstone
+ * @version : v1.0.0
+ * @since : 2023/4/17 10:15
+ */
+@Setter
+@Getter
+public class NukeBaseParam {
+    /**
+     * 指令库名称
+     */
+    @JsonProperty("__lib")
+    String lib;
+    /**
+     * 操作名称
+     */
+    @JsonProperty("__act")
+    String action;
+}   
