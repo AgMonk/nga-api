9aa04d0654488d4c3c69cb3bb50790c41169044e
ginstone <hjg719@139.com>
2023-06-09 12:50:36 +0800

    私信回复解析正文

--- src/main/java/com/gin/nga/response/field/pm/PrivateMessageReply.java
+++ src/main/java/com/gin/nga/response/field/pm/PrivateMessageReply.java
@@ -3,10 +3,13 @@ package com.gin.nga.response.field.pm;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.gin.common.serializer.ZdtJsonSerializer;
+import com.gin.nga.bbscode.entity.BbsTag;
+import com.gin.nga.bbscode.utils.BbsTagParser;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 
 /**
  * 私信回复
@@ -40,4 +43,7 @@ public class PrivateMessageReply {
     @JsonSerialize(using = ZdtJsonSerializer.class)
     ZonedDateTime timestamp;
 
+    public List<BbsTag> getContentNodes() {
+        return content == null ? null : BbsTagParser.parseContent(content);
+    }
 }
