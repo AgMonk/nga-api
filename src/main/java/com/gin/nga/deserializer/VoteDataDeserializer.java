00f6057c488e6e0483058a350d66ccd94394a20f
ginstone-home <hjg719@139.com>
2023-09-12 20:40:41 +0800

    1.0.14-beta
    投票数据为空时返回null

--- src/main/java/com/gin/nga/deserializer/VoteDataDeserializer.java
+++ src/main/java/com/gin/nga/deserializer/VoteDataDeserializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.gin.jackson.utils.ObjectUtils;
 import com.gin.nga.response.vote.BetData;
 import com.gin.nga.response.vote.BaseVoteData;
 import com.gin.nga.response.vote.VoteData;
@@ -21,6 +22,9 @@ import java.io.IOException;
 public class VoteDataDeserializer extends JsonDeserializer<BaseVoteData> {
 
     public static BaseVoteData parse(String s) {
+        if (ObjectUtils.isEmpty(s)){
+            return null;
+        }
         //        是否为菠菜
         final boolean isBet = s.contains("type~1");
         return isBet ? new BetData(s) : new VoteData(s);
