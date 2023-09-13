0a930ef4afc0ac4c41d49a2d033091e42ed8f7f8
ginstone <hjg719@139.com>
2023-09-13 16:15:51 +0800

    1.0.15-beta
    任务 - 增加是否可签到的判断方法
    
    修正 - 用户信息中的字段含义

--- src/main/java/com/gin/nga/response/field/user/UserInfoNuke.java
+++ src/main/java/com/gin/nga/response/field/user/UserInfoNuke.java
@@ -2,6 +2,7 @@ package com.gin.nga.response.field.user;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.gin.nga.deserializer.BooleanDeserializer;
 import com.gin.nga.deserializer.UserForumDeserializer;
 import com.gin.nga.deserializer.UserRemarkNukeDeserializer;
 import com.gin.nga.enums.UserMoreInfoType;
@@ -65,8 +66,14 @@ public class UserInfoNuke extends BaseUserInfo {
     /**
      * 关注数
      */
-    @JsonAlias("follow")
+    @JsonAlias("follow_num")
     Integer followCount;
+    /**
+     * 是否已关注
+     */
+    @JsonAlias("follow")
+    @JsonDeserialize(using = BooleanDeserializer.class)
+    Boolean followed;
     /**
      * 被关注数
      */
@@ -119,8 +126,8 @@ public class UserInfoNuke extends BaseUserInfo {
      * 总互动数
      * @return 总互动数
      */
-    public Long getInteractions(){
-        if (moreInfo==null){
+    public Long getInteractions() {
+        if (moreInfo == null) {
             return null;
         }
         final UserMoreInfo info = moreInfo.values().stream().filter(i -> i.getType() == UserMoreInfoType.interactions).findFirst().orElse(null);
