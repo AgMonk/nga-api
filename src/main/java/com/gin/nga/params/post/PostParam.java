6534065ff8824c933608e187bae43c1f604f896c
ginstone <hjg719@139.com>
2023-09-14 10:40:55 +0800

    1.0.20-beta
    发帖参数增加: 同步到动态

--- src/main/java/com/gin/nga/params/post/PostParam.java
+++ src/main/java/com/gin/nga/params/post/PostParam.java
@@ -12,6 +12,8 @@ import com.gin.nga.deserializer.BooleanDeserializer;
 import com.gin.nga.enums.Hidden;
 import com.gin.nga.response.body.UploadBody;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -30,7 +32,15 @@ import java.util.stream.Collectors;
  * @since : 2023/4/20 11:07
  */
 @Getter
+@Setter
+@NoArgsConstructor
 public class PostParam {
+    //
+    private static final int OPT_VOTE_REPLY_VIEW = 2048;
+    //
+    private static final int OPT_VOTE_END_VIEW = 4096;
+    //同步关注动态(仅在回复时
+    private static final int OPT_REPLY_FOLLOW_PUSH = 8192;
     /**
      * 单贴回复参数
      */
@@ -50,36 +60,36 @@ public class PostParam {
      * 标题
      */
     @JsonProperty("post_subject")
-    final String title;
+    String title;
     /**
      * 正文
      */
     @JsonProperty("post_content")
-    final String content;
+    String content;
     /**
      * (仅对超过编辑时限的回复) 添加到末尾,当准备请求返回的同名字段为1时需要设置为true
      */
     @JsonProperty("modify_append")
     @JsonSerialize(using = BooleanJsonSerializer.class)
     @JsonDeserialize(using = BooleanDeserializer.class)
-    final boolean modifyAppend;
+    boolean modifyAppend;
     /**
      * 隐藏内容,版主可见
      */
     @JsonProperty("hidden")
-    final Hidden hidden;
+    Hidden hidden;
     /**
      * 匿名发帖
      */
     @JsonProperty("anony")
     @JsonSerialize(using = BooleanJsonSerializer.class)
     @JsonDeserialize(using = BooleanDeserializer.class)
-    final boolean anony;
+    boolean anony;
     /**
      * 附件列表
      */
     @JsonIgnore
-    final List<UploadBody> attachs = new ArrayList<>();
+    List<UploadBody> attachs = new ArrayList<>();
     /**
      * at 用户
      */
@@ -88,8 +98,23 @@ public class PostParam {
 
     @JsonProperty("tpic_misc_bit1")
     Integer topicMiscBit;
+    /**
+     * 发帖选项 <a href="https://img4.nga.178.com/common_res/js_postfunc_v2.js">参考</a>
+     */
+    @JsonProperty("post_opt")
+    Integer postOpt;
 
-    public PostParam(String title, String content, boolean modifyAppend, Hidden hidden, boolean anony, boolean replyOnce) {
+    /**
+     * 发帖参数
+     * @param title 标题
+     * @param content 正文
+     * @param modifyAppend 是否添加到末尾
+     * @param hidden 隐藏选项
+     * @param anony 是否匿名
+     * @param replyOnce 是否发布为单回复贴
+     * @param replyFollowPush 是否同步到动态
+     */
+    public PostParam(String title, String content, boolean modifyAppend, Hidden hidden, boolean anony, boolean replyOnce, boolean replyFollowPush) {
         content = ObjectUtils.isEmpty(content) ? "" : content;
 
         this.title = title;
@@ -99,8 +124,13 @@ public class PostParam {
         this.anony = anony;
 
         //单帖回复(发布新主题时使用)
-        if (replyOnce){
+        if (replyOnce) {
             topicMiscBit = REPLY_ONCE;
+        }
+
+        if (replyFollowPush) {
+            postOpt = postOpt != null ? postOpt : 0;
+            postOpt = postOpt | OPT_REPLY_FOLLOW_PUSH;
         }
 
         // 处理 at 代码
@@ -121,7 +151,7 @@ public class PostParam {
      * @param content 正文
      */
     public PostParam(String title, String content) {
-        this(title, content, false, null, false, false);
+        this(title, content, false, null, false, false,false);
     }
 
     /**
@@ -130,7 +160,7 @@ public class PostParam {
      * @param content 正文
      */
     public PostParam(String content) {
-        this(null, content, false, null, false, false);
+        this(null, content, false, null, false, false,false);
     }
 
     /**
@@ -140,10 +170,10 @@ public class PostParam {
      */
     public static String encodeUnicode(String raw) {
         StringBuilder sb = new StringBuilder();
-        UnicodeUtils.unicodeIterator(raw, (index, codePoint, s,unicode) -> {
+        UnicodeUtils.unicodeIterator(raw, (index, codePoint, s, unicode) -> {
             final Language lang = Language.findLanguage(codePoint);
             //4位16进制上限
-            if (useUnicode(lang,codePoint)) {
+            if (useUnicode(lang, codePoint)) {
                 sb.append(unicode);
             } else {
                 sb.append(s);
@@ -159,18 +189,18 @@ public class PostParam {
      * @param codePoint codePoint
      * @return 是否使用unicode
      */
-    public static boolean useUnicode(Language lang,int codePoint){
+    public static boolean useUnicode(Language lang, int codePoint) {
         if (UNICODE_LANGUAGE.contains(lang)) {
             return true;
         }
         if (NOT_UNICODE_LANGUAGE.contains(lang)) {
             return false;
         }
-        if (lang==Language.other){
-            if (codePoint < 256){
+        if (lang == Language.other) {
+            if (codePoint < 256) {
                 return false;
             }
-            if (codePoint > 65535){
+            if (codePoint > 65535) {
                 return true;
             }
             return true;
